22f1be6c178e643103eb7900ffd082db49e70811
Luke Warner <lukeswarner@gmail.com>
2024-04-09 14:02:10 -0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src='https://www.collectivegoods.com/wp-content/uploads/CG-logo.png' alt="Collective Goods logo" />
+        </a>
+      </div>
+      <h1>Hello Adam</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
