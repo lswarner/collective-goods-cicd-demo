1328f11d0b920f491affabdd4b5e0aaae8280c9a
Luke Warner <lukeswarner@gmail.com>
2024-04-09 22:55:13 -0600

    verifying staging and prod deployments

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ function App() {
         </a>
       </div>
       <h1>Hello Adam</h1>
-      <p>This should update STAGING only. Attempt 2.</p>
-     
+      <p>Here's a CI/CD pipeline with staging and production environments.</p>
+      <p>When a pull request into the `main` github branch is closed, the `staging` environment is built and deployed.</p>
+      <p>After testing in stage, a new PR deploys a release to production by opening a PR from `main` into the `prodution`. This triggers the `production` environment to build and deployed.</p>
     </>
   )
 }
